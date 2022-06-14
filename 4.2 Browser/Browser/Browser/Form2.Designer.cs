2274b0a55688c6a10d83745693592465a7d4f596
Rui Simes <santossimoes.ruimiguel@gmail.com>
2022-06-14 19:12:30 +0100

    acho que já está fixe bom trabalho joana!

---
+++ 4.2 Browser/Browser/Browser/Form2.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Browser
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.Label();
+            this.encID = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(654, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Produtos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Encomenda";
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(353, 32);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectButton.TabIndex = 14;
+            this.SelectButton.Text = "Select";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.AutoSize = true;
+            this.TextBox.Location = new System.Drawing.Point(82, 36);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(118, 16);
+            this.TextBox.TabIndex = 13;
+            this.TextBox.Text = "ID da Encomenda:";
+            // 
+            // encID
+            // 
+            this.encID.Location = new System.Drawing.Point(226, 33);
+            this.encID.Name = "encID";
+            this.encID.Size = new System.Drawing.Size(98, 22);
+            this.encID.TabIndex = 12;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(657, 97);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(536, 418);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(85, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(540, 418);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 547);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SelectButton);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.encID);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.Label TextBox;
+        private System.Windows.Forms.TextBox encID;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ 4.2 Browser/Browser/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    
+    public partial class Form1 : Form
+    {
+        public static string conString = "", user = "";
+        public Form1()
+        {
+            InitializeComponent();
+            hostName.Text = "192.168.100.14";
+            dataBaseName.Text = "ABD_TRAB1";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string host_name = hostName.Text, data_base_name = dataBaseName.Text, password = userPassword.Text;
+            user = userName.Text;
+
+            conString = "Data Source=" + host_name + "; Initial Catalog = " + data_base_name + "; User ID = " + user + "; Password = " + password;
+            SqlConnection DataBaseConnection = new SqlConnection(conString);
+
+            //try to connect né é a vida
+            try
+            {
+                DataBaseConnection.Open();
+                MessageBox.Show("Connected!");
+
+                DataBaseConnection.Close();
+                Form2 form2 = new Form2();
+                form2.Show();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Button clickedButton = (Button)sender;
+                clickedButton.Text = " Tentar Conectar Novamente... ";
+                clickedButton.Enabled = true;
+            };
+        }
+    }
+}
